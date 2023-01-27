b4b3143401caa3950c234d8b86f294875b3ce916
Колесникова Алёна Сергеевна <241911@mck-ktits.ru>
2023-01-27 13:12:21 +0300

    FoodHouse

--- src/components/MainBanner/MainBanner.jsx
+++ src/components/MainBanner/MainBanner.jsx
@@ -8,7 +8,7 @@ function MainBanner() {
             <div className={mainBannerStyle.mainBanner__content}>
                 <div className={mainBannerStyle.mainBanner__contentTop}>
                     <p className={mainBannerStyle.mainBanner__contentTopText}>Экспресс-доставка от Яндекс.Еды</p>
-                    <h1 className={mainBannerStyle.mainBanner__contentTopTitle}>Быстро и вкусно <span>.</span></h1>
+                    <h1 className={mainBannerStyle.mainBanner__contentTopTitle}>Быстро и вкусно<span>.</span></h1>
                     <p className={mainBannerStyle.mainBanner__contentButtonText}>Сеть быстрого питания #1 в Казани</p>
                     <div className={mainBannerStyle.mainBanner__buttonText}>
                         <a href="#">Сделать заказ</a>
